9fa51a70768fc4157bcff68de4072a6596c815bd
JoacoMontandon <62565901+JoacoMontandon@users.noreply.github.com>
2020-05-14 17:19:06 -0300

    Update Property.cs

--- src/Library/Property.cs
+++ src/Library/Property.cs
@@ -1,5 +1,13 @@
 namespace Library
 {
+    
+    /*
+    Aplicamos el patrón Expert y los principios SRP y OCP.
+ 
+    Creamos una clase abstracta Property de tipo IProperty dado que no queremos que haya una instancia de Property pero si que clases que comparten comportamiento puedan heredar y sobrescribir solamente el código necesario. De esta manera se reutiliza código y se mantiene un código extensible, por ende cumple con OCP.
+    La única responsabilidad de la clase Property es almacenar las características de las propiedades, entonces su única razón de cambio es si se la agregan o modifican atributos, por lo tanto cumple con el principio SRP y a su vez sigue el patrón Expert porque se le asigna la responsabilidad a la clase que tiene toda la información necesaria. 
+*/
+
     public abstract class Property : IProperty
     {
         public string Neighborhood { get; set; }
